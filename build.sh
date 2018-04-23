dd78897b21a5882f4671146535ce1bcd2f98e2ed
Andrew Branch <andrew@wheream.io>
2018-04-22 22:01:13 -0700

    Add footnote about revision, even though link isn’t working

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COMMIT="$(git rev-parse --short HEAD)"
-docker run -v $DIR/src:/data:ro -v $DIR/dist:/out harshjv/texlive-2015 pdflatex -output-directory /out /data/resume.tex
+docker run -e "COMMIT=$COMMIT" -v $DIR/src:/data:ro -v $DIR/dist:/out harshjv/texlive-2015 pdflatex -output-directory /out /data/resume.tex && \
 cp dist/resume.pdf "dist/resume-$COMMIT.pdf"
