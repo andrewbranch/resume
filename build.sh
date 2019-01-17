0cb249e72c2e5b18803dd3ad6fed2feea01e3234
Andrew Branch <andrew@wheream.io>
2019-01-16 23:23:26 -0800

    Do cp within docker

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COMMIT="$(git rev-parse --short HEAD)"
-docker run -e "COMMIT=$COMMIT" -v $DIR/src:/data:ro -v $DIR/dist:/out harshjv/texlive-2015 pdflatex -output-directory /out /data/resume.tex && \
-cp dist/resume.pdf "dist/resume-$COMMIT.pdf"
+docker run -e "COMMIT=$COMMIT" -v $DIR/src:/data:ro -v $DIR/dist:/out harshjv/texlive-2015 sh -c "pdflatex -output-directory /out /data/resume.tex && cp /out/resume.pdf /out/resume-$COMMIT.pdf"
